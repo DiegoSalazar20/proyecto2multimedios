acf4718e86203b24c6317206da0fd1f33de5e016
RitchelS <sojoritchel@gmail.com>
2025-06-22 14:19:37 -0600

    Página de contactos

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../menu/menu.component';
+
+interface Contacto {
+  nombre: string;
+  correo: string;
+  descripcion?: string;
+  telefono?: string;
+}
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, MenuComponent],
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  styleUrls: ['./contacto.component.css']
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
+  contactos: Contacto[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('assets/emprendimientos.json').subscribe((data) => {
+      if (data?.contactos_adicionales) {
+        this.contactos = data.contactos_adicionales.filter((c: Contacto) => !!c.nombre);
+      } else {
+        console.error('contactos_adicionales no encontrados en el JSON');
+      }
+    });
+  }
 }
