f460a81734e56c65647c2929e91c5d2f15081328
Dreynei <drey0224@gmail.com>
2025-06-22 15:21:07 -0600

    image json

--- src/app/emprendimientos/emprendimientos.component.ts
+++ src/app/emprendimientos/emprendimientos.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MenuComponent} from '../menu/menu.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-emprendimientos',
-  imports: [],
+  imports: [
+    MenuComponent,
+  ],
   templateUrl: './emprendimientos.component.html',
   styleUrl: './emprendimientos.component.css'
 })
