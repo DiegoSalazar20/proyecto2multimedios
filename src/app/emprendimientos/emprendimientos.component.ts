3d216ebe35f4caf11cc58cde85e00c0e4c5e6b9b
Dreynei <drey0224@gmail.com>
2025-06-22 16:08:06 -0600

    get emprendimientos

--- src/app/emprendimientos/emprendimientos.component.ts
+++ src/app/emprendimientos/emprendimientos.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MenuComponent} from '../menu/menu.component';
-import {NgForOf} from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+export interface Emprendimiento {
+  nombre: string;
+  categoria: string;
+  descripcion: string;
+  ubicacion: string;
+  horario: string;
+  contacto: Contacto;
+  precio: string;
+  imagenes: Imagen[];
+}
+
+export interface Contacto {
+  telefono: string;
+  correo: string;
+  web: string;
+}
+
+export interface Imagen {
+  url_image: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-emprendimientos',
-  imports: [
-    MenuComponent,
-  ],
+  imports: [CommonModule, HttpClientModule, MenuComponent],
   templateUrl: './emprendimientos.component.html',
   styleUrl: './emprendimientos.component.css'
 })
-export class EmprendimientosComponent {
+export class EmprendimientosComponent implements OnInit {
+  emprendimientos: Emprendimiento[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('assets/emprendimientos.json').subscribe((data) => {
+      if (data?.emprendimientos) {
+        this.emprendimientos = data.emprendimientos.filter((e: Emprendimiento) => !!e.nombre);
+      } else {
+        console.error('contactos_adicionales no encontrados en el JSON');
+      }
+    });
+  }
 
 }
