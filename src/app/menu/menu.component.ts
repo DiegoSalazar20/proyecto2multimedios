2c488f915dc892010be4794b7a937aa8653606b7
DiegoSalazar20 <dsalazarzamora2001@gmail.com>
2025-06-21 18:28:35 -0600

    Pagina de inicio y menu desktop

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  rutaActual: string = '';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.rutaActual = this.router.url;
+    });
+  }
+
+  redirigir(ruta: string) {
+    this.router.navigate([ruta]);
+  }
 }
